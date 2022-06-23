e845b6577122c1baf7dd55390aa691e8994772cb
Omkar Kulkarni <omkar@Omkars-MacBook-Pro.local>
2022-06-23 17:18:13 -0400

    add test file

--- tfile.py
+++ tfile.py
@@ -3,6 +3,7 @@ import pathlib
 
 path = pathlib.Path(__file__).resolve()
 parent_path = pathlib.Path(__file__).parent.resolve()
+parent_parent_path = pathlib.Path(__file__).parent.parent.resolve()
 
 print("Path: ", path)
 print("Parent directory path: ", parent_path)
